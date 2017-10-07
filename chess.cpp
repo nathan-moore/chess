0d99a9a4737406100765f5e29108f18f942590bb
nathan-moore <nathanmoore424@gmail.com>
2017-10-07 00:07:16 -0400

    started user io

--- chess.cpp
+++ chess.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <algorithm>
 
 #include "board.h"
+
+int parse(std::string str,Square* to,Square* from);
+bool cmp(int x);
 
 void newgame()
 {
@@ -28,7 +33,45 @@ void newgame()
           Board.printboard();
 
           std::cout << "It is currently " << turn_name << "\'s turn" << std::endl;
-          std::cin >> input;
-          return;
+
+          std::string input;
+          Square to;
+          Square from;
+          int status;
+
+          do{
+               std::string temp;
+               std::getline(std::cin,temp);
+               std::getline(std::cin,input);
+
+               status = parse(input,&to,&from);
+          }while(status != 0);
+
+          std::cout << "I got here" << std::endl;
      }
+}
+
+int parse(std::string str,Square* to,Square* from)
+{
+     std::transform(str.begin(),str.end(),str.begin(),::tolower);
+
+     char first = str[0];
+     from -> x = first - 'a';
+
+     first = str[3];
+     to -> x = first - 'a';
+
+     from -> y = str[1] - '0';
+     to -> y = str[4] - '0';
+
+     if(cmp(from -> x) || cmp(from -> y) || cmp(to -> x) || cmp(to -> y))
+     {
+          return 1;
+     }
+     return 0;
+}
+
+bool cmp(int x)
+{
+     return (x > 0) && (x < 8);
 }
